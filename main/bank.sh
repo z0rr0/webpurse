05eb9947359aefd4ce9faa9958f16f6018943e3e
Alexander Zaycev (laptop) <thebestzorro@ya.ru>
2012-03-10 17:46:38 +0400

    change import bank

--- main/bank.sh
+++ main/bank.sh
@@ -6,7 +6,9 @@ else
     appdir=$1
     # get file
     cd $appdir
+	source ./env/bin/activate
     /usr/bin/wget -O bank.xml http://www.cbr.ru/scripts/XML_daily.asp
     # update file
-    /usr/bin/python manage.py bankupdate
+    ./env/bin/python manage.py bankupdate
+	deactivate
 fi
